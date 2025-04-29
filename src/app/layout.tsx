810a4f593b4c5cac2439b6e353226b7de6f6248f
exxedlight <exxedlight@aol.com>
2025-04-29 20:51:48 +0300

    Added boxicons locally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'/>
+        <link href='/boxicons.min.css' rel='stylesheet'/>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
